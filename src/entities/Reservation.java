5e8f7bd15bbad16deb2ec50e33bb412f77addb35
Wanley Rooby Alexis <alexroob96@gmail.com>
2025-01-29 17:45:02 -0300

    Reservation

---
+++ src/entities/Reservation.java
@@ -0,0 +1,66 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	// Exibir as datas em um formato padrão
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation() {
+	}
+
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	// Calcular a duração de uma reserva em dias
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	// Atualizar as datas de check-in e check-out
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		// Retornar uma string formatada com os detalhes da reserva
+		return "Room " 
+				+ roomNumber 
+				+ "check-in: " 
+				+ sdf.format(checkIn) 
+				+ "check-out: " 
+				+ sdf.format(checkOut) 
+				+ duration() 
+				+ "nights";
+	}
+
+}
